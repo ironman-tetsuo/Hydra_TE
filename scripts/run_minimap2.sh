994fa2edfd46b5ef82835a84f0cb7ae6c6b1c02a
ironman-tetsuo <58040608+ironman-tetsuo@users.noreply.github.com>
2024-11-06 13:07:28 +0100

    Add files via upload

---
+++ scripts/run_minimap2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+thread=48
+reference_path="GENOME.fa"
+
+fastq_path=(path1 path2...)
+FileNames=(name1 name2...)
+SampleNames=(sample1 sample2...)
+
+for i in `seq 0 $((${#SampleNames[@]}-1))`; do
+minimap2 \
+-ax  map-ont \
+--secondary=no \
+${reference_path} \
+${fastq_path[${i}]}/${FileNames[${i}]} \
+-t ${thread} \
+| \
+samtools view -h -bS  | samtools sort -@ ${thread} > ${SampleNames[${i}]}.sort.bam
+samtools index ${SampleNames[${i}]}.sort.bam
+samtools flagstat -@ ${thread} ${SampleNames[${i}]}.sort.bam > ${SampleNames[${i}]}.sort.bam.flagstat
+samtools depth -a ${SampleNames[${i}]}.sort.bam > ${SampleNames[${i}]}.sort.bam.depth
+samtools stats -@ ${thread} ${SampleNames[${i}]}.sort.bam > ${SampleNames[${i}]}.sort.bam.stats
+samtools view -h -bS -q 60 ${SampleNames[${i}]}.sort.bam > ${SampleNames[${i}]}.q60.sort.bam
+samtools index ${SampleNames[${i}]}.q60.sort.bam
+samtools flagstat -@ ${thread} ${SampleNames[${i}]}.q60.sort.bam > ${SampleNames[${i}]}.q60.sort.bam.flagstat
+samtools depth -a ${SampleNames[${i}]}.q60.sort.bam > ${SampleNames[${i}]}.q60.sort.bam.depth
+done
