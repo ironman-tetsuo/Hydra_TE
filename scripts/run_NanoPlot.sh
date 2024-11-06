bd4d3184dd92e4d68aaada5187d5c90c5b7dfa67
ironman-tetsuo <58040608+ironman-tetsuo@users.noreply.github.com>
2024-11-06 12:50:46 +0100

    Add files via upload

---
+++ scripts/run_NanoPlot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Declare variables
+thread=1
+fastq_path=(path1 path2...)
+FileNames=(file1 file2...)
+SampleNames=(name1 name2)
+
+#<<COMMENT
+for i in `seq 0 $((${#SampleNames[@]}-1))`; do
+NanoPlot \
+--fastq ${fastq_path[${i}]}/${FileNames[${i}]}  \
+--loglength  \
+-t ${thread}  \
+-o ${SampleNames[${i}]}
+done
