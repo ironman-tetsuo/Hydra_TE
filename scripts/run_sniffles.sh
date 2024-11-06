e2d6ae245a307a123c622824f3c7af8397909612
ironman-tetsuo <58040608+ironman-tetsuo@users.noreply.github.com>
2024-11-06 13:09:29 +0100

    Add files via upload

---
+++ scripts/run_sniffles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Variables
+bam_path=(path1 path2...)
+SampleNames=(sample1 sample2...)
+
+#Perform SV calling for each sample
+for i in `seq 0 $((${#SampleNames[@]}-1))`; do
+sniffles \
+--input ${bam_path}/${SampleNames[${i}]}.q60.sort.bam \
+--snf ${SampleNames[${i}]}.q60.sort.bam.snf \
+--reference ${reference_path} \
+--threads ${thread} \
+--output-rnames \
+--minsupport 2
+done
+
+
+#Generate array describing file names for variant calling
+SnfFileNames=("${SampleNames[@]}")
+#Add suffix
+SnfFileNames=( "${SnfFileNames[@]/%/.q60.sort.bam.snf}" )
+#Add prefix of file paths
+SnfFileNames=( "${SnfFileNames[@]/#/${snf_path}/${MyOutDir[${i}]}/}" )
+
+#merge Snf files 
+sniffles \
+--threads ${thread} \
+--combine-low-confidence 0.01 --combine-low-confidence-abs 1 --combine-null-min-coverage 3 \
+--input ${SnfFileNames[@]} \
+--vcf multisample.vcf
+
+#Compress vcf file
+gzip -c multisample.vcf > multisample.vcf.gz
